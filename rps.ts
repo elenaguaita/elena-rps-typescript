d8908d856c263c003ce04fdf5618d6bc8a59d284
elenaguaita <e.elenaguaita@gmail.com>
2025-05-28 17:48:51 +0200

    (refactor) Add calculateResult function with pattern matching

--- rps.ts
+++ rps.ts
@@ -1,7 +1,17 @@
 import { createInterface } from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
+import { match, P } from "ts-pattern";
 
 const rl = createInterface({ input, output });
+
+const calculateResult = (userMove: string, computerMove: string) =>
+  match([userMove, computerMove])
+    .with(["0", "2"], ["1", "0"], ["2", "1"], () => rl.write("You win!"))
+    .with(
+      P.when(([u, c]) => u === c),
+      () => rl.write("It's a draw.")
+    )
+    .otherwise(() => rl.write("You lose..."));
 
 export async function play(question: string) {
   const userMove = (await rl.question(question)).trim();
@@ -10,16 +20,7 @@ export async function play(question: string) {
   if (!["0", "1", "2"].includes(userMove)) rl.write("That's not a valid move.");
   else {
     rl.write(`You played: ${userMove}\nComputer played: ${computerMove}\n`);
-    if (computerMove === userMove) rl.write("It's a draw.");
-    else if (
-      (userMove === "0" && computerMove === "2") ||
-      (userMove === "1" && computerMove === "0") ||
-      (userMove === "2" && computerMove === "1")
-    ) {
-      rl.write("You win!");
-    } else {
-      rl.write("You lose...");
-    }
+    calculateResult(userMove, computerMove);
   }
 }
 
