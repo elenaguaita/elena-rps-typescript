cb1bf769defb913a74324e757cbdc719174f14eb
elenaguaita <e.elenaguaita@gmail.com>
2025-05-29 15:24:38 +0200

    Add unit tests using vitest

---
+++ tests/rps.test.ts
@@ -0,0 +1,71 @@
+import { describe, it, expect, test, afterEach, vi } from "vitest";
+import { calculateResult, generateRandomMove } from "../rps";
+import { Move, read } from "../types/move";
+
+describe("read", () => {
+  test.each([
+    ["0", "Rock"],
+    ["1", "Paper"],
+    ["2", "Scissors"],
+  ])('if user inputs %s, should parse it into "%s"', (userInput, expected) => {
+    expect(read(userInput)).toBe(expected);
+  });
+
+  it("should return null for invalid inputs", () => {
+    const invalidInputs = [
+      // Add here other invalid inputs
+      "3",
+      "-1",
+      "a",
+      "",
+      "rock",
+      " ",
+      "null",
+      "undefined",
+      "NaN",
+    ];
+
+    for (const input of invalidInputs) {
+      expect(read(input)).toBeNull();
+    }
+  });
+});
+
+describe("generateRandomMove", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test.each([
+    [0.0, "Rock"],
+    [0.4, "Paper"],
+    [0.8, "Scissors"],
+  ])(
+    'if Math.random returns %d, should return "%s"',
+    (randomOutput, expected) => {
+      vi.spyOn(Math, "random").mockReturnValue(randomOutput);
+      expect(generateRandomMove()).toBe(expected);
+    }
+  );
+});
+
+describe("calculateResult", () => {
+  test.each([
+    ["Rock", "Scissors", "You win!"],
+    ["Paper", "Rock", "You win!"],
+    ["Scissors", "Paper", "You win!"],
+    ["Scissors", "Rock", "You lose..."],
+    ["Rock", "Paper", "You lose..."],
+    ["Paper", "Scissors", "You lose..."],
+    ["Rock", "Rock", "It's a draw."],
+    ["Paper", "Paper", "It's a draw."],
+    ["Scissors", "Scissors", "It's a draw."],
+  ])(
+    'if user plays %s and computer plays %s, should return "%s"',
+    (userMove, computerMove, expected: string) => {
+      expect(calculateResult(userMove as Move, computerMove as Move)).toBe(
+        expected
+      );
+    }
+  );
+});
